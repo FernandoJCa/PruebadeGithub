519ab142590192eb83243989e1f68b2df2a1fb7b
Fernando Cardozo <cardozof123@outlook.com>
2024-02-14 09:49:29 -0500

    Commit de rama que va a morir

--- Prueba/src/main/java/org/example/Main.java
+++ Prueba/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
     System.out.println("Por queee?");
     System.out.println("Por que te quejas?");
     System.out.println("Hola, esta rama morira dentro de 5 min");
+    System.out.println("Hola, esta rama morira dentro de 5 min");
   }
 }
