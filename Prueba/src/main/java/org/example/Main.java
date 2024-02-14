c2e9971580957eea9660bc3b6c13fd280bf2d91b
Fernando Cardozo <cardozof123@outlook.com>
2024-02-14 09:42:57 -0500

    Commit de prueba

--- Prueba/src/main/java/org/example/Main.java
+++ Prueba/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Por que?");
+    System.out.println("Por queee?");
     System.out.println("Por que te quejas?");
   }
 }
